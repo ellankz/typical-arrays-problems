6d0187ba40390344248a368f1eba5073c5918b68
ellankz <enelipa@gmail.com>
2020-09-30 14:18:27 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+exports.min = function min(array = []) {
+    return array.reduce((least, current) => {
+        return current < least ? current : least;
+    }, 0);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array = []) {
+    return array.reduce((most, current) => {
+        return current > most ? current : most;
+    }, 0);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array = []) {
+    const sum = array.reduce((total, current) => {
+        return total + current;
+    }, 0);
+    return array.length > 0 ? sum / array.length : 0;
+};
